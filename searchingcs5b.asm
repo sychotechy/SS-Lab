8aac6df419080c02bb5ad0a5d70f77d13a7a2365
sychotechy <111345069+sychotechy@users.noreply.github.com>
2024-11-05 10:54:59 +0530

    Searching assembly language program added

---
+++ searchingcs5b.asm
@@ -0,0 +1,30 @@
+
+.stack 100h
+.data
+    text db 'Hello World'
+    count dw 13
+    search db 'r'
+    found db 'String Found$'
+    notfound db 'String NOT Found$' 
+.code
+begin:
+    mov ax,@data
+    mov ds,ax
+    mov es,ax
+    mov cx,count
+    mov di,offset text
+    mov al,search
+    repne scasb
+    jz yes
+    mov dx,offset notfound     
+    mov ah,09
+    int 21h
+    jmp over
+yes:
+    mov dx,offset found
+    mov ah,09
+    int 21h
+over:
+    mov ah,4ch
+    int 21h
+    end begin         
